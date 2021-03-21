8053dd5e2b95fda69a6991ebe430daec922d650a
Kaike Soares <kaike.soares@hotmail.com>
2021-03-21 11:18:55 -0300

    Primeira classe: Position

--- chess-game/src/application/Program.java
+++ chess-game/src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		Position posicao = new Position(3, 2);
+		System.out.println(posicao);
 
 	}
 
