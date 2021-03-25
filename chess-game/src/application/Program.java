48748f8487142d33f27912aaedfb4bafa4684524
Kaike Soares <kaike.soares@hotmail.com>
2021-03-24 23:56:05 -0300

    Starting to implement Board and Piece

--- chess-game/src/application/Program.java
+++ chess-game/src/application/Program.java
@@ -1,13 +1,12 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position posicao = new Position(3, 2);
-		System.out.println(posicao);
+		Board board = new Board(8, 8);
 
 	}
 
